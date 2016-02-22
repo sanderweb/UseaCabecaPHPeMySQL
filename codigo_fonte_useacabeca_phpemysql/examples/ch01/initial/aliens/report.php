44d6888e9416237c1a47c6c3b4d7f8dc3f4bafe3
sanderweb <alexsand1>
2016-02-22 17:23:45 -0300

    Parei na PG 43
    Parei na página 43

--- codigo_fonte_useacabeca_phpemysql/examples/ch01/initial/aliens/report.php
+++ codigo_fonte_useacabeca_phpemysql/examples/ch01/initial/aliens/report.php
@@ -2,27 +2,33 @@
 <html lang="pt-br">
 <head>
 <meta charset="UTF-8">
-<title>Aliens Abducted Me - Report an Abduction</title>
+<title>Aliens Abduziram  o meu cachorro - Você viu o meu cachorro?</title>
 <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
-    <h2>Aliens Abducted Me - Report an Abduction</h2>
+    <h2>Aliens Abduziram  o meu cachorro - Você viu o meu cachorro?</h2>
         <?php
-            $whe_it_heppened = $_POST['whnithappened'];
-            $how_long = $POST['holong'];
-            $alien_description = $POST['description'];
+            $primeiro_nome = $_POST['primeironome'];
+            $sobre_nome = $_POST['sobrenome'];     
+            $whe_it_heppened = $_POST['whenithappened'];
+            $how_long = $_POST['howlong'];
+            $how_many = $_POST['howmany'];
+            $alien_description = $_POST['aliendescription'];
+            $what_theydid = $_POST['whattheydid'];
             $fang_spotted =$_POST['fangspotted'];
             $email = $_POST['email'];
+            $comment = $_POST['other'];
     
-            echo 'Obrigado por enviar o formulário.<br/>';
-            echo 'Você foi sequestrado'. $whe_it_heppened;
-            echo 'E se foram para'. $how_long . '<br/>';
-            echo 'Descreva-os:' . $alien_description .'<br/>';
-            echo 'Estava presa lá?' .$fang_spotted. '<br/>';
-            echo 'O seu endereço de e-mail é' . $email;
-            echo '<br/>';          
-            
-        ?>    
-
+            echo 'Obrigado por enviar o formulário.<br/><br/>';
+            echo 'Nome: '. $primeiro_nome.' '.$sobre_nome. '<br/>';
+            echo 'Você foi sequestrado: '. $whe_it_heppened.'<br/>';
+            echo 'E quanto tempo: '. $how_long . '<br/>';
+            echo 'E você viu: '. $how_many. '<br/>';
+            echo 'Descreva-os: ' . $alien_description .'<br/>';
+            echo 'E fizeram com você? ' .$what_theydid. '<br/>';
+            echo 'Bunda Rachada estava lá? ' .$fang_spotted. '<br/>';
+            echo 'O seu endereço de e-mail é: ' . $email. '<br/>';
+            echo 'Comentário... ' .$comment;
+        ?>
 </body>
 </html>
